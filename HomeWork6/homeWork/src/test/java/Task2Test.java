61187a5ffb59f963c6c8734a1f9a86693ecc4df2
Lighter <agniage@list.ru>
2021-09-15 18:05:27 +0300

    /

--- HomeWork6/homeWork/src/test/java/Task2Test.java
+++ HomeWork6/homeWork/src/test/java/Task2Test.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static junit.framework.TestCase.assertTrue;
-
+//
 public class Task2Test {
     private static final Logger log = LoggerFactory.getLogger(Task2Test.class);
 
